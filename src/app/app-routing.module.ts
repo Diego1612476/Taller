33b3c3360363c200882b51cc297b4698b18e993b
taty <TatyanaVa@users.noreply.github.com>
2023-11-10 20:44:56 -0500

    cambios tatiana

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TerminosComponent } from './screens/terminos/terminos.component';
+import { ProductosComponent } from './screens/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'productos',component:ProductosComponent},
+  {path:'terminos',component:TerminosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

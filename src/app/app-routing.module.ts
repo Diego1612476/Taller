97a2d13225d51c37f2f052756334853c57838193
Diego1612476 <dalejarcia@gmail.com>
2023-11-14 20:35:41 -0500

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './screens/home/home.component';
 import { UsuarioComponent } from './screens/usuario/usuario.component';
 import { Error404Component } from './screens/error404/error404.component';
+import { TerminosComponent } from './screens/terminos/terminos.component';
+import { ProductosComponent } from './screens/productos/productos.component';
 
 const routes: Routes = [
 
   {path: 'home', component:HomeComponent},
   {path: 'usuario', component:UsuarioComponent},
 
-  {path: "**", component:Error404Component}
-=======
-import { TerminosComponent } from './screens/terminos/terminos.component';
-import { ProductosComponent } from './screens/productos/productos.component';
+  {path: "**", component:Error404Component},
 
-const routes: Routes = [
+
+
   {path:'productos',component:ProductosComponent},
   {path:'terminos',component:TerminosComponent}
->>>>>>> 80ab930b706800a2df470cad815a328aa028c9a0
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './screens/home/home.component';
 import { UsuarioComponent } from './screens/usuario/usuario.component';
 import { Error404Component } from './screens/error404/error404.component';
 import { FooterComponent } from './components/footer/footer.component';
-=======
 import { ProductosComponent } from './screens/productos/productos.component';
 import { TerminosComponent } from './screens/terminos/terminos.component';
->>>>>>> 80ab930b706800a2df470cad815a328aa028c9a0
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     NavBarComponent,
     HomeComponent,
     UsuarioComponent,
     Error404Component,
-    FooterComponent
-=======
+    FooterComponent,
     ProductosComponent,
     TerminosComponent
->>>>>>> 80ab930b706800a2df470cad815a328aa028c9a0
   ],
   imports: [
     BrowserModule,

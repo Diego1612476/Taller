6671e5a37e43dd2a5ebb375d7631841ee1b232b3
taty <TatyanaVa@users.noreply.github.com>
2023-11-14 20:02:58 -0500

    cambio3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { HomeComponent } from './screens/home/home.component';
 import { UsuarioComponent } from './screens/usuario/usuario.component';
 import { Error404Component } from './screens/error404/error404.component';
+import { TerminosComponent } from './screens/terminos/terminos.component';
+import { ProductosComponent } from './screens/productos/productos.component';
 
 const routes: Routes = [
 
   {path: 'home', component:HomeComponent},
   {path: 'usuario', component:UsuarioComponent},
 
-  {path: "**", component:Error404Component}
-=======
-import { TerminosComponent } from './screens/terminos/terminos.component';
-import { ProductosComponent } from './screens/productos/productos.component';
-
-const routes: Routes = [
   {path:'productos',component:ProductosComponent},
-  {path:'terminos',component:TerminosComponent}
->>>>>>> 80ab930b706800a2df470cad815a328aa028c9a0
-];
+  {path:'terminos',component:TerminosComponent},
+  {path: "**", component:Error404Component}
 
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]

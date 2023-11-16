0f0a0f4fa29c781cd6deb145e2239940742199d9
Diego1612476 <dalejarcia@gmail.com>
2023-11-15 20:56:27 -0500

    2

--- src/app/screens/usuario/usuario.component.ts
+++ src/app/screens/usuario/usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-usuario',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent {
+
+  constructor(private servicio:UsuarioService){}
+
+  dataUsuario:any={};
+
+  gOnInit(){
+    this.servicio.getUsuario().subscribe(usuario => {
+      this.dataUsuario = usuario
+    })
+  }
+
+  guardar (nombre:string, username:string, email:string){
+
+    const temp={
+      nombre:nombre,
+      username:username,
+      email:email
+
+    }
+
+    this.servicio.postUsuario(temp).subscribe()
+
+    alert("REGISTRADO CORRECTAMENTE")
+  }
 
 }
